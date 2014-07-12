971cb4c11d29116f031cb284bc596b6197128ff3
tiansh <tiansheng111@gmail.com>
2014-07-12 20:18:11 +0800

    0.1

--- Yet_Another_Weibo_Filter.user.js
+++ Yet_Another_Weibo_Filter.user.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name        Yet Another Weibo Filter
 // @namespace   https://github.com/tiansh
-// @description 新浪微博根据关键词、作者、话题、来源过滤微博；改造版面。 filter Sina Weibo by keywords, original, topic, and source; reform layout
+// @description 新浪微博根据关键词、作者、话题、来源等过滤微博；修改版面。 新浪微博根據關鍵字、作者、話題、來源等篩選微博；修改版面。 filter Sina Weibo by keywords, original, topic, source, etc.; modify layout
 // @include     http://weibo.com/*
 // @include     http://www.weibo.com/*
 // @version     0.1.10 alpha
+// @updateURL   https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.meta.js
+// @downloadURL https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.user.js
+// @author      田生
+// @copyright   2013+, 田生
+// @license     The MIT Licenses (MIT); http://opensource.org/licenses/MIT
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setValue
 // @grant       GM_getValue
