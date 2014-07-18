9aa5d9340d2bba056870c68f6abda8fbbc405dc3
tiansh <tiansheng111@gmail.com>
2014-07-18 15:04:01 +0800

    “应用”“热门”弄反了……

--- Yet_Another_Weibo_Filter.user.js
+++ Yet_Another_Weibo_Filter.user.js
@@ -4,7 +4,7 @@
 // @description 新浪微博根据关键词、作者、话题、来源等过滤微博；修改版面。 新浪微博根據關鍵字、作者、話題、來源等篩選微博；修改版面。 filter Sina Weibo by keywords, original, topic, source, etc.; modify layout
 // @include     http://weibo.com/*
 // @include     http://www.weibo.com/*
-// @version     0.1.22 alpha
+// @version     0.1.23 alpha
 // @updateURL   https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.meta.js
 // @downloadURL https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.user.js
 // @author      田生
@@ -1424,8 +1424,8 @@ var layouts = (function () {
 
   subtitle('Nav');
   item('Main', '.gn_nav>div:nth-child(1) { display: none !important; }');
-  item('App', '.gn_nav>div:nth-child(2) { display: none !important; }');
-  item('Hot', '.gn_nav>div:nth-child(3) { display: none !important; }');
+  item('Hot', '.gn_nav>div:nth-child(2) { display: none !important; }');
+  item('App', '.gn_nav>div:nth-child(3) { display: none !important; }');
   item('Game', '.gn_nav>div:nth-child(4) { display: none !important; }');
   item('Member', '.gn_setting[node-type="member"] { display: none !important; }');
 
