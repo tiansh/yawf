58749aecbacb2f997122e11a5207dc9649b0b306
tiansh <tiansheng111@gmail.com>
2014-07-18 15:08:04 +0800

    24 手滑了，无视…… >_<

--- Yet_Another_Weibo_Filter.meta.js
+++ Yet_Another_Weibo_Filter.meta.js
@@ -4,7 +4,7 @@
 // @description 新浪微博根据关键词、作者、话题、来源等过滤微博；修改版面。 新浪微博根據關鍵字、作者、話題、來源等篩選微博；修改版面。 filter Sina Weibo by keywords, original, topic, source, etc.; modify layout
 // @include     http://weibo.com/*
 // @include     http://www.weibo.com/*
-// @version     0.1.23 alpha
+// @version     0.1.24 alpha
 // @updateURL   https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.meta.js
 // @downloadURL https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.user.js
 // @author      田生
