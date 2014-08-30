b41b9f30d882406fe023666e50f0b2652c20688f
tiansh <tiansheng111@gmail.com>
2014-08-30 22:56:22 +0800

    63

--- Yet_Another_Weibo_Filter.meta.js
+++ Yet_Another_Weibo_Filter.meta.js
@@ -1,24 +1,30 @@
 // ==UserScript==
-// @name        Yet Another Weibo Filter
-// @namespace   https://github.com/tiansh
-// @description 新浪微博根据关键词、作者、话题、来源等过滤微博；修改版面。 新浪微博根據關鍵字、作者、話題、來源等篩選微博；修改版面。 filter Sina Weibo by keywords, authors, topics, sources, etc.; modify layout
-// @include     http://www.weibo.com/*
-// @include     http://weibo.com/*
-// @exclude     http://weibo.com/a/bind/test
-// @version     1.2.62
-// @updateURL   https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.meta.js
-// @downloadURL https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.user.js
-// @supportURL  https://tiansh.github.io/yawf/
-// @author      田生 http://weibo.com/tsh90
-// @copyright   田生; The MIT License
-// @license     The MIT License (MIT); http://opensource.org/licenses/MIT
-// @grant       GM_xmlhttpRequest
-// @grant       GM_setValue
-// @grant       GM_getValue
-// @grant       GM_deleteValue
-// @grant       GM_addStyle
-// @grant       GM_registerMenuCommand
-// @grant       GM_info
-// @grant       unsafeWindow
-// @run-at      document-start
+// @name              Yet Another Weibo Filter
+// @name:zh           看真正想看的微博 (YAWF)
+// @name:en           Yet Another Weibo Filter
+// @namespace         https://github.com/tiansh
+// @description       新浪微博根据关键词、作者、话题、来源等过滤微博；修改版面。 新浪微博根据关键词、作者、话题、来源等过滤微博；修改版面。 filter Sina Weibo by keywords, authors, topics, sources, etc.; modify layout
+// @description:zh-CN 新浪微博根据关键词、作者、话题、来源等过滤微博；修改版面。
+// @description:zh-HK 新浪微博根據關鍵字、作者、話題、來源等篩選微博；修改版面。
+// @description:zh-TW 新浪微博根據關鍵字、作者、話題、來源等篩選微博；修改版面。
+// @description:en    filter Sina Weibo by keywords, authors, topics, sources, etc.; modify layout
+// @include           http://www.weibo.com/*
+// @include           http://weibo.com/*
+// @exclude           http://weibo.com/a/bind/test
+// @version           1.2.63
+// @updateURL         https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.meta.js
+// @downloadURL       https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.user.js
+// @supportURL        https://tiansh.github.io/yawf/
+// @author            田生 http://weibo.com/tsh90
+// @copyright         田生; The MIT License
+// @license           The MIT License (MIT); http://opensource.org/licenses/MIT
+// @grant             GM_xmlhttpRequest
+// @grant             GM_setValue
+// @grant             GM_getValue
+// @grant             GM_deleteValue
+// @grant             GM_addStyle
+// @grant             GM_registerMenuCommand
+// @grant             GM_info
+// @grant             unsafeWindow
+// @run-at            document-start
 // ==/UserScript==
