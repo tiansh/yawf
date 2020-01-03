be6a48a30b5a75873100cd535d750d2ea105c182
田生 <tiansheng111@gmail.com>
2020-01-04 00:42:58 +1100

    57: 关闭聊天界面不显示提示

--- Yet_Another_Weibo_Filter.meta.js
+++ Yet_Another_Weibo_Filter.meta.js
@@ -12,7 +12,7 @@
 // @description:zh-TW Yet Another Weibo Filter (YAWF) 新浪微博根據關鍵詞、作者、話題、來源等篩選微博；修改版面
 // @description:en    Sina Weibo feed filter by keywords, authors, topics, source, etc.; Modifying webpage layout
 // @namespace         https://github.com/tiansh
-// @version           4.0.55
+// @version           4.0.57
 // @match             https://*.weibo.com/*
 // @include           https://weibo.com/*
 // @include           https://*.weibo.com/*
@@ -23,7 +23,6 @@
 // @exclude           https://security.weibo.com/*
 // @exclude           https://verified.weibo.com/*
 // @exclude           https://vip.weibo.com/*
-// @exclude           https://api.weibo.com/chat*
 // @noframes
 // @run-at            document-start
 // @grant             GM.info
